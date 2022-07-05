9acb1db75ac380e17a507db84cb0d7b8270f41f5
lostgirljourney <falgunisarkar526@gmail.com>
2022-07-05 11:38:14 +0530

    deploy commit

--- src/db.cjs
+++ src/db.cjs
@@ -7,7 +7,10 @@ const proConfig = process.env.DATABASE_URL; //heroku addons
 
 const pool = new Pool({
   connectionString:
-    process.env.NODE_ENV === 'production' ? proConfig : devConfig
+    process.env.NODE_ENV === 'production' ? proConfig : devConfig,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
